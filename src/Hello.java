28796f60a71b7b2126c89cd287c736af7dfe38ab
gur99 <gur.yitzhaki@gmail.com>
2024-11-07 20:09:07 +0200

    first initial- hello world

---
+++ src/Hello.java
@@ -0,0 +1,25 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello bitches !!!");
+		int number1 =5;
+		double number2 = 15.5;
+		double sum ;
+		
+		sum = number1 + number2;
+		System.out.println(AddFifteen(sum));
+		
+	}
+
+	
+	
+	public static double AddFifteen(double number) {
+		double f = 15+ number;
+		return f;
+	}
+	
+	
+}
